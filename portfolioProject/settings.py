bb0a82cff93e8b33f41ba8d6ed53666ea78329f6
Michelle <michelleirenemcfarland@gmail.com>
2020-09-08 17:19:02 -0700

    Fixed static path

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/Users/michellemcfarland/portfolio/blog/static/'
+STATIC_URL = os.path.join(BASE_DIR, "blog/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = ("/Users/michellemcfarland/portfolio/blog/static/", )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "blog/static/"),)
